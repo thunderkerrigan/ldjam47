175c9fccbeaf9b0e4b4f08d924e18e7c90628ed8
tribaleur <tribaleur@yahoo.fr>
2020-10-04 17:32:06 +0200

    Update SpawnerPathFollower

--- Assets/Scripts/Railcar/SpawnerPathFollower.cs
+++ Assets/Scripts/Railcar/SpawnerPathFollower.cs
@@ -5,35 +5,36 @@ using UnityEngine;
 
 public class SpawnerPathFollower : MonoBehaviour {
 
-    public GameObject   _trainPrefab;
+    public GameObject   _defaultTrainPrefab;
 	[Range(0.1f, 5f)]
-	public float        _trainSpeed = 0.5f;
+	public float        _defaultTrainSpeed = 0.5f;
     public PathCreator  _pathCreator;
-	[Range(1f, 300f)]
+	private Vector3		_startPosition;
+
+	/*[Range(1f, 300f)]
     public int          _spawnTimeSeconde = 20;
 	[Range(1f, 300f)]
     public int          _firstSpawnTimeSeconde = 5;
 
 	private CountDown	_countDownSpawn;
 	private CountDown	_countDownFirstSpawn;
-	private bool		_isFirstSpaned = false;
-	private Vector3		_startPosition;
+	private bool		_isFirstSpaned = false;*/
 
 	private void OnEnable () {	
 		// Check if the train prefab have a PathFollowerComponent
-		PathFollower_Tilled followerComponent = this._trainPrefab.GetComponent<PathFollower_Tilled>();
-		if(followerComponent == null) { _trainPrefab = null; }
-
+		PathFollower_Tilled followerComponent = this._defaultTrainPrefab.GetComponent<PathFollower_Tilled>();
+		if(followerComponent == null) { _defaultTrainPrefab = null; }
+		/*
 		this._countDownSpawn = new CountDown(this._spawnTimeSeconde);
 		this._countDownFirstSpawn = new CountDown(this._firstSpawnTimeSeconde);
-		this._countDownFirstSpawn.start();
+		this._countDownFirstSpawn.start();*/
 
 		if (this._pathCreator != null) { this._startPosition = this._pathCreator.path.GetPointAtDistance(0f, EndOfPathInstruction.Stop); }
 	}
 
-	// Update is called once per frame
-	void Update() {
-        if(this._trainPrefab != null && this._pathCreator && this._countDownFirstSpawn.IsUp) {
+	// Endless spawn
+	/*void Update() {
+        if(this._defaultTrainPrefab != null && this._pathCreator && this._countDownFirstSpawn.IsUp) {
 			// First spawn
 			if (!this._isFirstSpaned) {
 				this._isFirstSpaned = true;
@@ -47,14 +48,28 @@ public class SpawnerPathFollower : MonoBehaviour {
 				this._countDownSpawn.start();
 			}
 		}
-    }
+    }*/
 
-	private void spawn () {
-		GameObject newTrain = Instantiate(this._trainPrefab, this._startPosition, Quaternion.identity);
-		PathFollower_Tilled followerComponent = newTrain.GetComponent<PathFollower_Tilled>();
-		followerComponent._currentPath._pathCreator = this._pathCreator;
-		followerComponent._currentPath._pathWay = PathTile.PathWay.Start;
-		followerComponent._speed = this._trainSpeed;
-		newTrain.SetActive(true);
+	public void spawn () {
+		this.spawn(this._defaultTrainPrefab, this._defaultTrainSpeed);
+	}
+	
+	public void spawn (GameObject trainPrefab) {
+		this.spawn(trainPrefab, this._defaultTrainSpeed);
+	}
+
+	public void spawn (GameObject trainPrefab, float trainSpeed) {
+		// Check the if the Prefab
+		PathFollower_Tilled followerComponent = trainPrefab?.GetComponent<PathFollower_Tilled>();
+		if(followerComponent == null) { trainPrefab = _defaultTrainPrefab; }
+
+		if(trainPrefab != null) {
+			GameObject newTrain = Instantiate(trainPrefab, this._startPosition, Quaternion.identity);
+			followerComponent = newTrain.GetComponent<PathFollower_Tilled>();
+			followerComponent._currentPath._pathCreator = this._pathCreator;
+			followerComponent._currentPath._pathWay = PathTile.PathWay.Start;
+			followerComponent._speed = trainSpeed;
+			newTrain.SetActive(true);
+		}
 	}
 }
