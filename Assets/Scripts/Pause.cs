5a7088560c6380ce0a262ea932322f7eed8ffeee
Rancune <pitet.brian@gmail.com>
2020-10-04 11:26:02 +0200

    Gestion du Volume
    Slider Volume

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameIsPaused)
+        {
+            Resume();
+        }else
+        {
+            PauseStop();
+        }
+    }
+
+    public void Resume()
+    {
+       Time.timeScale = 1f;
+       GameIsPaused = false;
+    }
+
+    public void PauseStop()
+    {
+       Time.timeScale = 0f;
+       GameIsPaused = true;
+    }
+}
