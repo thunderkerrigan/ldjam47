9e5e66d8818baa2a67a9712c9cdd8ef61f5fae3a
Joseph Pasqualini <thunderkerrigan@gmail.com>
2020-10-05 15:02:14 +0200

    ### deraillement

---
+++ Assets/Scripts/Railcar/TrainController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainController : MonoBehaviour
+{
+
+    private Animator _animator;
+
+    private PathFollower_Tilled _follower;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = gameObject.GetComponentInChildren<Animator>();
+        _follower = gameObject.GetComponent<PathFollower_Tilled>();
+        _follower.KillMePleaseHandler += Kill;
+    }
+
+    private IEnumerator DeathSwitch()
+    {
+        _animator.SetTrigger("Death");
+        yield return new WaitForSeconds(2.0f);
+        Destroy(this.gameObject);
+    }
+    
+    private void Kill(GameObject myself)
+    {
+        StartCoroutine(DeathSwitch());
+        _follower.KillMePleaseHandler -= Kill;
+
+    }
+}
